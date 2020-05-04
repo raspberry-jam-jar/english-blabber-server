99f78691380a6d17d65b678c0731d46d88fd691a
Полина Попова <popova_pv@km-union.ru>
2020-05-04 21:16:20 +0300

    Add data migration for game skeleton application

---
+++ modules/game_skeleton/data/__init__.py
@@ -0,0 +1,9 @@
+from .gifts_data import GIFT_DATA
+from .hero_classes_data import HEROCLASS_DATA
+from .skills_data import SKILL_DATA, RULE_DATA, GRADATION_DATA, \
+    PENALTY_DATA
+
+__all__ = [
+    GIFT_DATA, HEROCLASS_DATA, SKILL_DATA, RULE_DATA, GRADATION_DATA,
+    PENALTY_DATA
+]
