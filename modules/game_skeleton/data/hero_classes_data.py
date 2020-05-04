cc3de1ff6fb6d9b861a9171c6e70f9b80cf54018
Полина Попова <popova_pv@km-union.ru>
2020-05-04 21:16:20 +0300

    Add data migration for game skeleton application

---
+++ modules/game_skeleton/data/hero_classes_data.py
@@ -0,0 +1,44 @@
+HEROCLASS_DATA = [
+    {
+        'id': 1,
+        'name': 'Абитуриент',
+        'parent_id': None,
+        'capacity': 100,
+        'is_draft': False
+    },
+    {
+        'id': 2,
+        'name': 'Студент',
+        'parent_id': 1,
+        'capacity': 100,
+        'is_draft': False
+    },
+    {
+        'id': 3,
+        'name': 'Бакалавр',
+        'parent_id': 2,
+        'capacity': 100,
+        'is_draft': False
+    },
+    {
+        'id': 4,
+        'name': 'Магистр',
+        'parent_id': 3,
+        'capacity': 100,
+        'is_draft': False
+    },
+    {
+        'id': 5,
+        'name': 'Доктор',
+        'parent_id': 4,
+        'capacity': 100,
+        'is_draft': False
+    },
+    {
+        'id': 6,
+        'name': 'Профессор',
+        'parent_id': 5,
+        'capacity': 100,
+        'is_draft': False
+    },
+]
