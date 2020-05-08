ba6b3a796023575e7b7b8cbf57de31a9db5ef99b
Полина Попова <popova_pv@km-union.ru>
2020-05-08 17:40:47 +0300

    Dockerize server

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # english-blabber-server
 
+### Installation
+1. Clone the repository.
+    ```bash
+    https://github.com/raspberry-jam-jar/english-blabber-server.git --depth 1
+    ```
+2. Enable default server configuration.
+    ```bash
+    cd english-blabber-server/docker
+    cp .env.template .env
+    ```
+3. Run Docker containers.
+    ```bash
+    cd english-blabber-server/docker
+    docker-compose up -d
+    ```
+    If you used default configuration server in now available at `https://0.0.0.0:8000`.
+
+Use `docker-compose down` command to stop containers.
--- conf/urls.py
+++ conf/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 import class_room.urls
@@ -47,3 +49,8 @@ urlpatterns = [
         include((class_room.urls, 'class_room'), namespace='class_room')
     )
 ]
+
+if settings.DEBUG:
+    urlpatterns += \
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
